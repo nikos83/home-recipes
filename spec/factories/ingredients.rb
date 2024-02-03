5105a2d30d4b7bb55474ad45da928ec7d7951f05
Nikodem Górak <nikos83@gmail.com>
2024-02-03 13:06:06 +0100

    remove unit from ingredient

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -6,13 +6,11 @@
 #
 #  id         :bigint           not null, primary key
 #  name       :string
-#  unit       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 FactoryBot.define do
   factory :ingredient do
     name { "MyString" }
-    unit { "MyString" }
   end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -6,7 +6,6 @@
 #
 #  id         :bigint           not null, primary key
 #  name       :string
-#  unit       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
